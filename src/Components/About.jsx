b4cbe67f66fc931fc7816c340dc3d8da8b3f7412
Rishabh Tripathy <112729251+RishabhTripathy@users.noreply.github.com>
2022-11-15 21:49:07 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -12,21 +12,11 @@ const About = () => {
         <p className=" text text-4xl font-bold inline border-b-4 border-gray-500 ">About</p>
         </div>
         <p className="text-xl mt-10 ">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, magni
-        fugit mollitia repellendus quidem deserunt sit recusandae? Sint,
-        reiciendis excepturi at facilis quos voluptates tenetur fuga eos aperiam
-        laboriosam? Quisquam facere architecto explicabo molestiae ex quasi
-        itaque omnis dolor, nostrum eveniet id recusandae placeat amet illo nam
-        quia error autem!
+      Hello there! I’m a Website Designer & Developer living in Raipur,Chhattisgarh,India. Since my youngest age I have been passionate about design and art. I grew up with the Internet, and naturally took a path according to this passion. Web design is a way for me to keep track of all my passions. See ya!
       </p>
       <br />
       <p className="text-xl ">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
-        voluptates suscipit tenetur quae. Deleniti libero maxime eveniet. Nihil
-        totam, enim tempore vero ex pariatur quam expedita minus tenetur quas
-        dicta, possimus fugit eos repellat blanditiis in iure laudantium
-        nesciunt autem amet soluta eligendi reprehenderit? Deserunt cumque
-        adipisci enim inventore mollitia?
+        With a strong emphasis on "Progressive Enhancement", I look for creative ways to push the boundaries of website front-end code without compromising on browser support and performance.
       </p>
       </div>
      
