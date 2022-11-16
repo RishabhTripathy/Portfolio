e5ab6a621d80692b643006f0b9c5bbffe832a885
Rishabh Tripathy <112729251+RishabhTripathy@users.noreply.github.com>
2022-11-16 18:26:24 +0530

    Update Skills.jsx

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -75,9 +75,9 @@ const Skills = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline ">
             My Skills
           </p>
-          <p className="py-6 ">These are the technologies I've worked with.</p>
+          <p className="py-4 ">These are the technologies I've worked with.</p>
         </div>
-        <div className=" w-full grid grid-cols-2 sm:grid-cols-3  gap-6 text-center py-8 px-12 sm:px-0  ">
+        <div className=" w-full grid grid-cols-3 sm:grid-cols-3  gap-6 text-center py-6 px-6 sm:px-0  ">
           
           
           
